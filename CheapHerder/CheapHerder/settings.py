3aba86ba239ee9714eac10138043a71df7d9b544
Abdallah <hussein6@fa17-cs411-52.cs.illinois.edu>
2017-11-14 06:43:41 -0600

    uncommitted old commits

--- CheapHerder/CheapHerder/settings.py
+++ CheapHerder/CheapHerder/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'CheapHerder'
 ]
 
 MIDDLEWARE = [
@@ -79,8 +80,8 @@ DATABASES = {
        	'NAME': 'cheapherder',
    	'USER': 'admin',
         'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
