93b84d6fc314cfa7771b8fc877d5365b428d9261
Hussein Abdallah <hussein@hussein-abdallah.ubanquity.io>
2017-11-15 14:15:37 +0200

    crawler fix + models fix

--- CheapHerder/CheapHerder/models.py
+++ CheapHerder/CheapHerder/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
 	supplier_id = models.ForeignKey("auth.User", limit_choices_to={'groups__name': "Suppliers"}, null=True)
 
 	def __str__(self):
-		return self.sku + '-' + self.title
+		return self.item_code + '-' + self.product_name
 
 class Price(models.Model):
 	price_id = models.AutoField(primary_key=True) 
